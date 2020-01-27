5bb33ce861db872b88f489728c190a14e0b5047c
Kyle Xyian Dilbeck <49416739+XYIAN@users.noreply.github.com>
2020-01-27 09:45:42 -0800

    Initial commit

---
+++ src/Lab00.java
@@ -0,0 +1,65 @@
+public class Lab00 {
+    /**
+     * This is a main method. Any class can have one.
+     * It is what runs when the the class is called from the command line.
+     *
+     * @param args These are the command line arguments that get passed in when the code runs
+     */
+    public static void main(String[] args) {
+        //TODO: Add a print statement
+//        also cmd+/ (ctrl+/ on a PC) will auto comment
+
+//        TODO: make mainLoop work!
+        mainLoop();
+
+    }
+
+    /**
+     * <p>This is a method. Specifically it is a static method.<br>
+     * What does private mean? <br>
+     * What does static?<br>
+     * What does int mean?<br>
+     *
+     * <p> This method will start a loop that presents a menu to the user.<br>
+     *  The menu will look like:<br>
+     *  <pre>
+     *      Welcome to the command line calculator!
+     *      Select a choice below:
+     *      0: Quit
+     *      1: add
+     *      2: subtract
+     *      3: multiply
+     *</pre>
+     * @return returns a number based on execution status (0 is success)
+     */
+    private static int mainLoop(){
+
+        //TODO: start a loop (a while loop probably makes the most sense)
+        //TODO: Ask for user input (We need scanner)
+        //TODO: use a try catch to prevent errors
+        //TODO: Use a switch statement to call one of the other methods from this class
+
+        return -1; // a negative return value may indicate what?
+    }
+
+    /**
+     * This method takes two numbers and returns the sum.
+     * @param x an integer
+     * @param y an integer
+     * @return returns an integer representing x+y;
+     */
+    private static int adder(int x, int y){
+        return 0;
+    }
+
+    /**
+     * Write a method named subtracter that takes two parameters and returns the difference of x and y
+     *
+     * @param x an integer
+     * @param y an integer
+     * @return returns x - y
+     */
+    // TODO: write the subtracter method here
+
+
+}
